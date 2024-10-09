e3c083f08a57facb303311b7d6713272e13499e8
Matan Nahmani <matanmatan999new@gmail.com>
2024-10-09 19:56:28 +0900

    update eslint

--- app/root.tsx
+++ app/root.tsx
@@ -1,24 +1,28 @@
+/**
+ * This is a remix project using shadcn and tailwindcss, framer motion, it's currently got everything configured and ready to go.
+ * you should use shadcn ui components to build your app use clean design and animations.
+ */
 import {
   Links,
   Meta,
   Outlet,
   Scripts,
   ScrollRestoration,
-} from "@remix-run/react";
-import type { LinksFunction } from "@remix-run/node";
+} from '@remix-run/react';
+import type { LinksFunction } from '@remix-run/node';
 
-import "./tailwind.css";
+import './tailwind.css';
 
 export const links: LinksFunction = () => [
-  { rel: "preconnect", href: "https://fonts.googleapis.com" },
+  { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
   {
-    rel: "preconnect",
-    href: "https://fonts.gstatic.com",
-    crossOrigin: "anonymous",
+    rel: 'preconnect',
+    href: 'https://fonts.gstatic.com',
+    crossOrigin: 'anonymous',
   },
   {
-    rel: "stylesheet",
-    href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
+    rel: 'stylesheet',
+    href: 'https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap',
   },
 ];
 
